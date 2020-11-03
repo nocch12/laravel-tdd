5a0059f049a13f008fa71b50291473a9e5aae7bd
Kohki Ohno <k.ohno12.gt@gmail.com>
2020-11-03 22:22:35 +0900

    コントローラーをリソースコントローラーに

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks.index', ['tasks' => $tasks]);
+    }
+
+    public function show(Task $task)
+    {
+        return view('tasks.show', ['task' => $task]);
+    }
+}
